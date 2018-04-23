1b5ecd2afd6c0fe83ebff9acdc84249b87a8dfc0
Ben Atkins <benjatkins13@gmail.com>
2018-04-23 10:35:46 -0500

    updated styling

--- dist/app.js
+++ dist/app.js
@@ -31,17 +31,17 @@ var blogs = [
               that we were required to use Javascript to populate content on each page. That seemed to be the most beneficial part of exercise the way I see it.
                I feel much more comfortable using '.innerHTML' method to dynamically show content. It seems like working through problems that seem impossible is
                 the most helpful for diving into each subsequent project or exercise</p>`},
-    {title: "Blog4",
-            date:"January 27th, 2017",
-            content:`<p>Sriracha try-hard godard, typewriter chicharrones meditation prism
-            shoreditch meggings tacos you probably haven't heard of them organic lyft.
-            Meditation brunch adaptogen cardigan vape shabby chic. Forage tilde trust fund 
-            drinking vinegar, raw denim irony pop-up vexillologist helvetica tumeric occupy
-            vegan. Succulents ramps readymade chambray. Hell of kitsch gluten-free lomo,
-                master cleanse pabst tumeric sriracha flexitarian. Fingerstache mlkshk y
-                ennui vice pickled ugh plaid post-ironic selvage retro single-origin coffee
-                normcore trust fund. Succulents tofu cronut affogato selvage poutine ugh 
-                chillwave raclette poke kogi.</p>`},
+    {title: "Chatty Pineapple Metal",
+            date:"February 14th, 2017",
+            content:`<p>Yesterday, we presented our latest group project. They just keep coming, but I have felt a strong
+            sense of satisfaction after each one of them. I imagine they are trying to get us in the mindset of actually having a job in this field
+            Anyways, this project required us as a group to create a mostly functioning chatting website. We called our site NSS Mayhem,
+            and it was meant to be a place to vent on days that programming just has it out for you.</p><p>The most difficult part of the project for me,
+            was developing a way to give each message bubble a dynamically created unique id, and associate that ID with a dynamically created button.
+            It didn't take me long to realize that Javascript does not like "document.getElementbyID" if the element doesn't exist yet. Anyways, I figured out
+            an ugly way to solve the problem by using a counter everytime a message is created and adding counter number to both the element id and the
+            dynamically created delete button. With this method, I was able to get the id of the button I clicked, then add the number at the end
+            of the name of the message and delete it that way. I felt pretty good about figuring that out</p>`},
     {title: "Blog5",
             date:"February 3rd, 2017",
             content:`<p>Sriracha try-hard godard, typewriter chicharrones meditation prism
